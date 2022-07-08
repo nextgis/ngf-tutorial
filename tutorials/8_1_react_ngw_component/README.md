dc02047fde199f2744eb02ed76646ddb980ba267
Artemiy <rendrom@gmail.com>
2022-07-08 16:26:25 +0200

    feat: add react category

---
+++ tutorials/8_1_react_ngw_component/README.md
@@ -0,0 +1,18 @@
+# 6.3. React. NgwMap
+
+## Theory
+
+## Practice
+
+To run this example, you need to execute these commands in the terminal:
+
+```bash
+npm i
+npm start
+```
+
+Look for the script output in the terminal.
+
+[LAUNCH](https://githubbox.com/nextgis/ngf-tutorial/tree/master/tutorials/7_1_extended_initialize_with_ready_map_instance) in the `codesandbox.io`.
+
+[BACK TO CONTENT](../../README.md)
---
+++ tutorials/8_1_react_ngw_component/src/main.js
@@ -0,0 +1,37 @@
+
+import React, {useRef} from 'react';
+import { render } from 'react-dom';
+import ReactNgwMap from '@nextgis/react-ngw-leaflet';
+import { MapControl, ToggleControl } from '@nextgis/react-ngw-map';
+
+
+function App() {
+  const ngwMap = useRef();
+
+  const mapOptions = {
+    baseUrl: 'https://demo.nextgis.com',
+    resources:[{resource: 6118, id: 'webmap', fit: true}],
+    whenCreated: (n) => { 
+      ngwMap.current = n;
+    }, 
+  }
+
+  const onClick = (status) => {
+    if (ngwMap.current) {
+      ngwMap.current.toggleLayer('webmap', status);
+    }
+  }
+
+  return <ReactNgwMap {...mapOptions} >
+    <ToggleControl html={{on: 'Hide', off: 'Show'}} onClick={onClick} position={'top-right'} status={true}/>
+    <MapControl position='bottom-right' margin>
+      <a href="https://nextgis.com" target="_blank"><img src="https://nextgis.com/img/nextgis.png" /></a>
+    </MapControl>
+  </ReactNgwMap>
+}
+
+
+
+const rootNode = document.getElementById('app');
+render(<App />, rootNode);
+
